e817fff598b45082aecc56cf1f39d48bb554b6b4
StiaanK <102803595+StiaanK@users.noreply.github.com>
2023-08-17 15:15:00 +0200

    fixed data

--- DataPreProcessing.py
+++ DataPreProcessing.py
@@ -2,32 +2,26 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 # Preparing data
-df = pd.read_csv('./adult.csv')
-
-# Create a copy of the dataframe with missing values dropped
-df_dropped = df.dropna()
-
-# Save the dataframe with dropped missing values to a new CSV file
-df_dropped.to_csv('./adult_dropped_missing.csv', index=False)
+df = pd.read_csv('C:\\Users\\Stiaan\\Desktop\\Honeurs\\Sem 2\\ITRW626\\AI-Group-Assignment\\Phase1\\adult.csv', delimiter=';')
 
 # Define feature columns and target column
-cols = ['age', 'workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'hoursperweek', 'native-country', 'class']
+cols = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'hours-per-week', 'native-country', 'income']
 
 # Encode categorical features using LabelEncoder
 label_encoders = {}
 encoding_info = {}
 
 for col in cols:
-    if df_dropped[col].dtype == 'object':
+    if df[col].dtype == 'object':
         le = LabelEncoder()
-        df_dropped[col] = le.fit_transform(df_dropped[col])
+        df[col] = le.fit_transform(df[col])
         label_encoders[col] = le
         
         # Create a dictionary to store encoding information
         encoding_info[col] = {original_value: encoded_value for original_value, encoded_value in zip(le.classes_, le.transform(le.classes_))}
         
 # Save the DataFrame with integer-encoded values to a new CSV file
-df_dropped.to_csv('./adult_encoded.csv', index=False)
+df.to_csv('./adult_encoded.csv', index=False)
 
 # Save encoding information to a text file
 with open('./encoding_info.txt', 'w') as file:
